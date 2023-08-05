98eacb1e6e0eb5dae6384d67fb5142ce3dbe531d
share knowledge <128280568+komding@users.noreply.github.com>
2023-08-05 21:30:40 +0530

    update

--- app.py
+++ app.py
@@ -1,71 +1,43 @@
-from flask import Flask, render_template, request, redirect, session
-import sqlite3
+import pandas as pd
+from flask import Flask, render_template, request
+import joblib
 
 app = Flask(__name__)
-app.secret_key = 'a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p'
 
-@app.route('/')
+# Load the model
+model = joblib.load('hpp.pkl')
+
+# Load and prepare the prediction_values.csv
+data = pd.read_csv('prediction_values.csv')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-@app.route('/codewithme', methods=['GET', 'POST'])
-def codewithme():
     if request.method == 'POST':
-        problem_name = request.form['problem_name']
-        problem_description = request.form['problem_description']
-        code = request.form['code']
-        category = request.form['category']
-        conn = sqlite3.connect('coding_problems.db')
-        c = conn.cursor()
-        c.execute("INSERT INTO problems (problem_name, problem_description, code, category) VALUES (?, ?, ?, ?)",
-                  (problem_name, problem_description, code, category))
-        conn.commit()
-        conn.close()
-        return redirect('/codewithme')
+        # Get user input values from the form
+        lot_area = float(request.form['lot_area'])
+        bedroom_abv_gr = int(request.form['bedroom_abv_gr'])
+        garage_cars = int(request.form['garage_cars'])
+        duplex = True if request.form['duplex'] == 'True' else False
+        overall_qual = int(request.form['overall_qual'])
+        fireplaces = int(request.form['fireplaces'])
+        pave = True if request.form['pave'] == 'True' else False
+        total_bsmt_sf = float(request.form['total_bsmt_sf'])
 
-    conn = sqlite3.connect('coding_problems.db')
-    c = conn.cursor()
-    c.execute("SELECT DISTINCT category FROM problems")
-    categories = c.fetchall()
-    c.execute("SELECT * FROM problems")
-    problems = c.fetchall()
-    conn.close()
+        # Update the data with user input
+        data.loc[0, 'LotArea'] = lot_area
+        data.loc[0, 'BedroomAbvGr'] = bedroom_abv_gr
+        data.loc[0, 'GarageCars'] = garage_cars
+        data.loc[0, 'Duplex'] = duplex
+        data.loc[0, 'OverallQual'] = overall_qual
+        data.loc[0, 'Fireplaces'] = fireplaces
+        data.loc[0, 'Pave'] = pave
+        data.loc[0, 'TotalBsmtSF'] = total_bsmt_sf
 
-    return render_template('codewithme.html', problems=problems, categories=categories)
+        # Make a prediction using the updated data
+        prediction = model.predict(data)
 
-@app.route('/delete/<int:problem_id>', methods=['POST'])
-def delete(problem_id):
-    conn = sqlite3.connect('coding_problems.db')
-    c = conn.cursor()
-    c.execute("DELETE FROM problems WHERE id = ?", (problem_id,))
-    conn.commit()
-    conn.close()
-
-    return redirect('/codewithme')
-
-@app.route('/viewcode')
-def viewcode():
-    conn = sqlite3.connect('coding_problems.db')
-    c = conn.cursor()
-    c.execute("SELECT DISTINCT category FROM problems")
-    categories = c.fetchall()
-    c.execute("SELECT * FROM problems")
-    problems = c.fetchall()
-    conn.close()
-
-    return render_template('viewcode.html', problems=problems, categories=categories)
-
-@app.route('/viewcode/<category_filter>')
-def viewcode_category(category_filter):
-    conn = sqlite3.connect('coding_problems.db')
-    c = conn.cursor()
-    c.execute("SELECT DISTINCT category FROM problems")
-    categories = c.fetchall()
-    c.execute("SELECT * FROM problems")
-    problems = c.fetchall()
-    conn.close()
-
-    return render_template('viewcode.html', problems=problems, categories=categories, category_filter=category_filter)
+        return render_template('index.html', prediction=prediction[0])
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
